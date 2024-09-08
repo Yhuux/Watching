20ccce8e6880ee1acebc0c6fb21dfb2cc5beacaf
Gabriel <116197351+Yhuux@users.noreply.github.com>
2024-09-08 12:45:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-///Test
+function openFullscreen(src) {
+    const fullscreenDiv = document.getElementById('fullscreenImage');
+    const fullscreenImg = fullscreenDiv.querySelector('img');
+    fullscreenImg.src = src;
+    fullscreenDiv.style.display = 'flex';
+}
+
+function closeFullscreen() {
+    document.getElementById('fullscreenImage').style.display = 'none';
+}
