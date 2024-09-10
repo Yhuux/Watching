d71f39cc3c2d9c85535d38cc55ec1de42e5f82dd
Gabriel <116197351+Yhuux@users.noreply.github.com>
2024-09-10 01:38:48 -0300

    Actualizar

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'image-cache-v1';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/optimized-script.js',
+                // Añade aquí otros recursos estáticos que quieras cachear
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request).then(response => {
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+                const responseToCache = response.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseToCache);
+                });
+                return response;
+            });
+        })
+    );
+});
