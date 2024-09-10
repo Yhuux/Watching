ff81152443c24c54790577bf8c1f6b24624e833c
Gabriel <116197351+Yhuux@users.noreply.github.com>
2024-09-10 01:38:07 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Constantes
 const CACHE_NAME = 'image-cache-v1';
 const IMAGE_URLS = [
-    'images/1.jpg', 'images/2.jpg', 'images/3.jpg', /* ... otros urls ... */ 'images/27.jpg'
+    'images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg',
+    'images/6.jpg', 'images/7.jpg', 'images/8.jpg', 'images/9.jpg', 'images/10.jpg',
+    'images/11.jpg', 'images/12.jpg', 'images/13.jpg', 'images/14.jpg', 'images/15.jpg',
+    'images/16.jpg', 'images/17.jpg', 'images/18.jpg', 'images/19.jpg', 'images/20.jpg',
+    'images/21.jpg', 'images/22.jpg', 'images/23.jpg', 'images/24.jpg', 'images/25.jpg',
+    'images/26.jpg', 'images/27.jpg'
 ];
 
 // Función para precargar imágenes en el caché
